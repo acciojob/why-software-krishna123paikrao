80e189e910107c15dc0c56d1a982de9c86f0cb6a
krishna123paikrao <116417631+krishna123paikrao@users.noreply.github.com>
2023-02-18 13:42:44 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-//your code here
-eb development gives you the opportunity 
-	to express yourself creatively on the internet. 
-		If you have an idea you would like to try as you 
-		discover your web dev skills, try it out. Web dev is
-			a fun, creative experience
